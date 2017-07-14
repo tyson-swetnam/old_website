a90c6868cc8a8bcb31e34a9279cbc0f1bc43f583
Tyson L. Swetnam <tswetnam@cyverse.org>
2017-07-14 12:15:46 -0700

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,27 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Flagstaff, Arizona, USA",
+    35.1762431, 
+    -111.6573040
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Flagstaff, Arizona, USA",
+    35.1901899,
+    -111.619448
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Santa Rita Experimental Range, Arizona, USA",
+    31.7611488,
+    -110.8450711
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Tucson, Arizona, USA",
+    32.2306465, 
+    -110.9532361
+  ]
+  [
+    "Portland, Oregon, USA",
+    45.5272391, 
+    -122.6625550
   ]
 ];
